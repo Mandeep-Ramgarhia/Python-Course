644f2eb8a3c50a537cb4f1d5ce786987b31cdfef
Mandeep kaur <137879997+Mandeep-Ramgarhia@users.noreply.github.com>
2024-08-03 17:07:44 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -1,1 +1,23 @@
+--> What is Programming
+Programming is a way for us to tell computers what to do. Computer is a very dumb machine and it only does what we tell it to do. Hence we learn programming and tell computers to do what we are very slow at - computation. If I ask you to calculate 5+6, you will immediately say 11. How about 23453453 X 56456?
+You will start searching for a calculator or jump to a new tab to calculate the same. 
 
+--> What is Python?
+1. Python is a dynamically typed, general purpose programming language that supports an object-oriented programming approach as well as a functional programming approach.
+2. Python is an interpreted and a high-level programming language.
+3. It was created by Guido Van Rossum in 1989.
+  
+--> Features of Python
+1. Python is simple and easy to understand.
+2. It is Interpreted and platform-independent which makes debugging very easy.
+3. Python is an open-source programming language.
+4. Python provides very big library support. Some of the popular libraries include NumPy, Tensorflow, Selenium, OpenCV, etc.
+5. It is possible to integrate other programming languages within python.
+  
+--> What is Python used for
+1. Python is used in Data Visualization to create plots and graphical representations.
+2. Python helps in Data Analytics to analyze and understand raw data for insights and trends.
+3. It is used in AI and Machine Learning to simulate human behavior and to learn from past data without hard coding.
+4. It is used to create web applications.
+5. It can be used to handle databases.
+6. It is used in business and accounting to perform complex mathematical operations along with quantitative and qualitative analysis.
