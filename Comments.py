af8491a14f2492ab69f7fe3624b51692eeb806e1
Mandeep kaur <137879997+Mandeep-Ramgarhia@users.noreply.github.com>
2024-08-04 18:37:25 +0530

    Comments.py

---
+++ Comments.py
@@ -0,0 +1,19 @@
+Single line Comments - 
+To write a comment just add a ‘#’ at the start of the line.
+  Example - 
+#This is a 'Single-Line Comment'
+print("This is a print statement.")
+  Output:
+  This is a print statement. 
+  
+  Multi-Line Comments:
+  To write multi-line comments you can use ‘#’ at each line or you can use the multiline string.
+  #It will execute a block of code if a specified condition is true.
+#If the condition is false then it will execute another block of code.
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+ Output:
+ p is greater than 5.
