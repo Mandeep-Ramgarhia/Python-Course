348d00ff4bf733e615e62cae217414f4a55c3872
Mandeep kaur <137879997+Mandeep-Ramgarhia@users.noreply.github.com>
2024-08-29 16:59:59 +0530

    22. Function.py

---
+++ 22. Function.py
@@ -0,0 +1,16 @@
+def calculateGmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a, b):
+    if(a>b):
+        print("First number is Greater")
+    else:
+         print("Second number is Greater or Equal")
+
+def isLesser(a, b):
+                pass
+a = 9
+b = 8
+isGreater(a, b)
+calculateGmean(a, b)
